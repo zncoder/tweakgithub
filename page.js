3a98538c1aa6d0a7fab4837e3ac19e1669c79e9f
Bin Zhang <zncoder@gmail.com>
2023-06-24 13:25:42 -0700

    observe mutation

--- page.js
+++ page.js
@@ -1,7 +1,15 @@
 // replace Issues with my pull requests
-let issues = document.querySelector('#issues-tab')
-if (issues) {
+function replaceIssues() {
+  let issues = document.querySelector('#issues-tab')
+  if (!issues) return false
+
   let text = issues.querySelector('span[data-content="Issues"]')
   text.innerText = "Pulls"
   issues.href = '/pulls'
+  return true
 }
+
+const obs = new MutationObserver(replaceIssues)
+obs.observe(document.body, {attributes: false, childList: true, subtree: false})
+
+replaceIssues()
