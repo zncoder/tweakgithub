9c5592138c773a90536e8a0b66853f19d427e8db
Bin Zhang <zncoder@gmail.com>
2023-06-30 14:21:15 -0700

    disconnect inside observe callback
    replace Actions with Requested
    
    replace code font

--- page.js
+++ page.js
@@ -1,15 +1,41 @@
-// replace Issues with my pull requests
-function replaceIssues() {
+function replaceIssuesWithPulls() {
   let issues = document.querySelector('#issues-tab')
-  if (!issues) return false
+  if (!issues) return
 
   let text = issues.querySelector('span[data-content="Issues"]')
   text.innerText = "Pulls"
   issues.href = '/pulls'
-  return true
 }
 
-const obs = new MutationObserver(replaceIssues)
-obs.observe(document.body, {attributes: false, childList: true, subtree: false})
+function replaceActionsWithRequested() {
+  let actions = document.querySelector('#actions-tab')
+  if (!actions) return
 
-replaceIssues()
+  let text = actions.querySelector('span[data-content="Actions"]')
+  text.innerText = "Requested"
+  issues.href = '/pulls/review-requested'
+}
+
+function replaceCodeFont() {
+  if (document.querySelector('blob-code-inner-tweakgithub')) return
+
+  for (let node of document.querySelectorAll('.blob-code-inner')) {
+    node.classList.add('blob-code-inner-tweakgithub')
+  }
+}
+
+const obs = new MutationObserver(tweak)
+
+function observe() {
+  obs.observe(document, {attributes: false, childList: true, subtree: true})
+}
+
+function tweak() {
+  obs.disconnect()
+  replaceIssuesWithPulls()
+  replaceActionsWithRequested
+  replaceCodeFont()
+  observe()
+}
+
+tweak()
